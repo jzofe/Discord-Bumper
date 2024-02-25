9cbf012160a1204033980ccfcff5efbb9c6b34fe
Cyber <55909183+jzofe@users.noreply.github.com>
2024-02-25 23:42:52 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
 
+
+OS=$(uname)
+
+
+if [ "$OS" == "Linux" ] && [ -f /etc/lsb-release ]; then
+    echo "OS : Ubuntu"
+    sudo apt-get install -y npm
+    sudo npm i cfonts -g
+elif [ "$OS" == "Linux" ] && [ -f /etc/redhat-release ]; then
+    echo "OS : CentOS"
+    sudo yum install -y npm
+    sudo npm i cfonts -g
+
+elif [ "$OS" == "Android" ]; then
+    echo "OS : Termux"
+    pkg install npm
+    npm i cfonts -g
+else
+    echo "[-]"
+fi
